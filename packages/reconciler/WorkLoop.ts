cb35e24293b1b9c3cf071fe315ca6e8c9d6b6f28
fengjiawang <fengjiawang@myhexin.com>
2025-11-16 02:10:07 +0800

    递归->循环

--- packages/reconciler/WorkLoop.ts
+++ packages/reconciler/WorkLoop.ts
@@ -2,12 +2,51 @@ import { beginWork } from "./BeginWork";
 import { completeWork } from "./CompleteWork";
 import { Fiber } from "./ReactInternalTypes";
 
+// 当前正在处理的节点
+let workInProgress: Fiber | null = null;
+
 /**
- * 遍历Fiber节点，完成对应工作
+ * 完成单元工作，对当前节点进行回溯阶段，并触发完成工作
+ * @param fiber
+ */
+function completeUnitOfWork(fiber: Fiber): void {
+  let completedWork: Fiber | null = fiber;
+  do {
+    completeWork(completedWork);
+    if (completedWork.sibling) {
+      workInProgress = completedWork.sibling;
+      return;
+    }
+    completedWork = completedWork.return;
+    workInProgress = completedWork;
+  } while (completedWork);
+}
+
+/**
+ * 执行单元工作，对当前节点进行向下遍历，并触发开始工作
+ * @param fiber
+ */
+function performUnitOfWork(fiber: Fiber): void {
+  let next = beginWork(fiber);
+  if (next) {
+    workInProgress = next;
+    return;
+  } else {
+    completeUnitOfWork(fiber);
+  }
+}
+
+/**
+ * 深度优先遍历Fiber树，执行工作
  * @param fiber
  * @returns {void}
  */
 export function workLoop(fiber: Fiber): void {
+  workInProgress = fiber;
+  while (workInProgress) {
+    // 向下的工作
+    performUnitOfWork(workInProgress);
+  }
   let child = beginWork(fiber);
   if (child) {
     workLoop(child);
